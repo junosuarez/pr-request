a4ef9ff841b35d0718d3a0da4a27e77deca05c1a
jden <jason@denizac.org>
2013-12-30 15:01:22 -0800

    upgrade to bluebird

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var Q = require('q')
+var Promise = require('bluebird')
 var r = require('request')
 
 var request = function (url) {
-  var deferred = Q.defer()
+  var deferred = Promise.defer()
   var req = r(url, function (err, res) {
     if (err) { deferred.reject(err) }
     else { deferred.resolve(res) }
